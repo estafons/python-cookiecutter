9cfc71d2c31af91ba0cc2acfc677df84ed44a3e7
estafons <stefanoskoutoupis@gmail.com>
2025-05-11 22:25:36 +0300

    fix on print config

--- {{cookiecutter.project_name}}/main.py
+++ {{cookiecutter.project_name}}/main.py
@@ -1,9 +1,10 @@
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
-@hydra.main(config_path="configs", config_name="config")
+@hydra.main(config_path="configs", config_name="config", version_base=None)
 def main(cfg: DictConfig):
-    print(cfg.pretty())
+    # print the configuration
+    print(OmegaConf.to_yaml(cfg))
 
 if __name__ == "__main__":
     main()
